d0c476496646b8a591f871dd052f1da05fd8abbb
taku10101 <taku1011ht@gmail.com>
2024-03-12 13:34:56 +0900

    fix: formProvider

--- src/form/formProvider.tsx
+++ src/form/formProvider.tsx
@@ -1,38 +1,29 @@
-import React from 'react'
-import {
-  useForm,
-  SubmitHandler,
-  FormProvider,
-  FieldValues,
-  DefaultValues,
-} from 'react-hook-form'
+import React from "react";
+import { useForm, SubmitHandler, FormProvider, FieldValues, DefaultValues } from "react-hook-form";
 
 type FormContainerProps<TFormValues extends FieldValues> = {
-  defaultValues: DefaultValues<TFormValues> | undefined
-  children: React.ReactNode
-  onSubmit: SubmitHandler<TFormValues>
-}
+  defaultValues: DefaultValues<TFormValues> | undefined;
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<TFormValues>;
+};
 
-export const formProvider=() => { 
-<TFormValues extends FieldValues>({
-  defaultValues,
-  children,
-  onSubmit,
-}: FormContainerProps<TFormValues>) => {
-  const methods = useForm<TFormValues>({
+export const formProvider = () => {
+  <TFormValues extends FieldValues>({
     defaultValues,
-  })
+    children,
+    onSubmit,
+  }: FormContainerProps<TFormValues>) => {
+    const methods = useForm<TFormValues>({
+      defaultValues,
+    });
 
-  return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        {children}
-        <button
-          type="submit">
-          送信
-        </button>
-      </form>
-    </FormProvider>
-  )
-}
-}
+    return (
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          {children}
+          <button type="submit">送信</button>
+        </form>
+      </FormProvider>
+    );
+  };
+};

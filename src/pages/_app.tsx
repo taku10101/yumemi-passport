49280f2a55e8d002a01fd0faab8133a2f8e62113
taku10101 <taku1011ht@gmail.com>
2024-03-15 01:10:56 +0900

    add: prefectures context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
+import { PrefectureProvider } from "@/store/prefectureContext";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PrefectureProvider>
+      <Component {...pageProps} />;
+    </PrefectureProvider>
+  );
 }
